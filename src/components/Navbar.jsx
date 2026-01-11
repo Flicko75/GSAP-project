7825fa4b4bc2191448b6265f83d9d11d91ea89c2
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2026-01-11 21:26:46 +0530

    Implement about section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
 
     return (
         <nav>
-            <div>
+            <div className="md:px-5">
                 <a href="#home" className="flex items-center gap-2">
                     <img src="/images/logo.png" alt="logo" />
                     <p>Velvet Pour</p>
